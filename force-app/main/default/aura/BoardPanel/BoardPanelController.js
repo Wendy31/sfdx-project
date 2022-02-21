b95d9f19b74c3c156a81a88eaaf68bf352e7cac3
Wendy <wendychung31@gmail.com>
2022-02-21 20:17:05 +0000

    added onclick events to buttons

--- force-app/main/default/aura/BoardPanel/BoardPanelController.js
+++ force-app/main/default/aura/BoardPanel/BoardPanelController.js
@@ -1,5 +1,9 @@
 ({
-    myAction : function(component, event, helper) {
+    startGame : function(component, event, helper) {
+        console.log("The start game function is called");
+    },
 
-    }
+    reshuffleBoard : function(component, event, helper) {
+        console.log("The shuffle board function is called");
+    },
 })
