86899437ddbedefc61d16082ca590c60d220a597
Wendy <wendychung31@gmail.com>
2022-02-22 16:58:57 +0000

    uniquely ID's component to access and use attributes using two-way data binding. Added new Board cmp and created layout for grids.

--- force-app/main/default/aura/BoardPanel/BoardPanelController.js
+++ force-app/main/default/aura/BoardPanel/BoardPanelController.js
@@ -1,6 +1,16 @@
 ({
     startGame : function(component, event, helper) {
-        console.log("The start game function is called");
+        // access combobox
+        let gameModeComboBox = component.find("gameMode");
+
+        // access value of combobox
+        let selectedValue = gameModeComboBox.get("v.value");
+
+        // update selectedMode attribute
+        component.set("v.selectedMode", selectedValue);
+
+        console.log("The start game function is called. The Game Mode is: " + selectedValue);
+        console.log("The selected Game Mode is: " + component.get("v.selectedMode"));
     },
 
     reshuffleBoard : function(component, event, helper) {
