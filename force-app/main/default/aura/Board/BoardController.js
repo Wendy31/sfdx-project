358671be91bbfb2f1737332c0496a4febf75f253
Wendy <wendychung31@gmail.com>
2022-02-23 18:03:02 +0000

    created array of words and new function to shuffle array in Helper file, to return a list of random words to Controller which is called by the init event handled in the Component file

--- force-app/main/default/aura/Board/BoardController.js
+++ force-app/main/default/aura/Board/BoardController.js
@@ -1,5 +1,8 @@
 ({
-    myAction : function(component, event, helper) {
+    doInit : function(component, event, helper) {
+        console.log("Initialization completed");
 
+        // get list of 100 words from helper file
+        console.log("Words: " + helper.getWords(6));
     }
 })
