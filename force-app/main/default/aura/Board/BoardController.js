983bcad9a8c20998a9c1f86b3760f57c911f6b88
Wendy <wendychung31@gmail.com>
2022-02-25 17:44:34 +0000

    created layout for grid per the game mode and added CSS styling

--- force-app/main/default/aura/Board/BoardController.js
+++ force-app/main/default/aura/Board/BoardController.js
@@ -2,20 +2,29 @@
     doInit : function(component, event, helper) {
         console.log("Initialization completed");
 
+        // set number of columns based on game mode
+        const gameMode = component.get("v.mode");
+        let column = 0;
+        if (gameMode && gameMode === "hard") {
+            column = 6;
+        } else if (gameMode === "medium") {
+            column = 4;
+        } else {
+            column = 3;
+        }
+
+        // set block size based on column
+        let blockSize = 12/ column;
+        component.set("v.blockSize", blockSize);
+
         // get list of words from helper file
-        const words = helper.getWords(6);
+        const words = helper.getWords(column * column);
         // set array of words to cmp
         component.set("v.words", words);
-        console.log("Words: " + words);
 
         // get random winWord
         const winWord = helper.getWinWord(words);
         // set WinWord to cmp
         component.set("v.winWord", winWord);
-        console.log("Win Word: " + winWord);
-        
-
-
-
     }
 })
